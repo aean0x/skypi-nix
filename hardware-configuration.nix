c8c7ef397b8149bcbeae91f91c2c1a1645b18b64
aean0x <3682177+aean0x@users.noreply.github.com>
2024-12-31 09:32:22 +0100

    Fix hardware configuration for SD card image building

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -69,15 +69,21 @@
   ###########################
   # File systems
   ###########################
-  fileSystems."/" = {
-    device = "/dev/mmcblk0p3";
-    fsType = "ext4";
-  };
+  fileSystems = {
+    "/" = lib.mkForce {
+      device = if config.sdImage.enable or false
+        then "/dev/disk/by-label/NIXOS_SD"
+        else "/dev/mmcblk0p3";
+      fsType = "ext4";
+    };
 
-  fileSystems."/boot/efi" = {
-    device = "/dev/mmcblk0p2"; # 300 MB EFI partition
-    fsType = "vfat";
-    options = ["fmask=0022" "dmask=0022"];
+    "/boot/efi" = lib.mkForce {
+      device = if config.sdImage.enable or false
+        then "/dev/disk/by-label/ESP"
+        else "/dev/mmcblk0p2";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
   };
 
   ###########################
